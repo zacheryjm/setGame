cdbc619d686a89ff9c7e89adb04b69c0afb2f672
zmiller <zmiller@cs.odu.edu>
2018-10-07 17:46:48 -0400

    Basic construction of set gameplay.

---
+++ SetGame/SetGame.swift
@@ -0,0 +1,125 @@
+//
+//  SetGame.swift
+//  SetGame
+//
+//  Created by Zachery Miller on 10/2/18.
+//  Copyright © 2018 Zachery Miller. All rights reserved.
+//
+
+import Foundation
+
+class SetGame {
+    
+    var deck = [Card]()
+    let maxNumberOfPlayableCards = 24
+    var playableCards = [Card]()
+    let maxNumberOfSelectedCards = 3
+    var score = 0
+    var selectedCards = [Int]()
+    
+    func dealThreeMoreCards() {
+        
+        for index in 0..<3 {
+            if !deck.isEmpty && playableCards.count < maxNumberOfPlayableCards{
+                playableCards.append(deck[index])
+                deck.remove(at: index)
+            }
+        }
+        
+        
+    }
+    
+    func chooseCard(at index : Int) {
+        
+        if selectedCards.count == maxNumberOfSelectedCards {
+            selectedCards.removeAll()
+            selectedCards.append(index)
+        }
+        else if selectedCards.count == maxNumberOfSelectedCards-1 {
+            selectedCards.append(index)
+            
+            if checkForSet() {
+                score += 1
+                
+                for i in selectedCards.indices {
+                    playableCards.remove(at: i)
+                    
+                    if !deck.isEmpty {
+                        playableCards.append(deck[i])
+                        deck.remove(at: i)
+                    }
+                    
+                }
+            }
+            else {
+                score -= 1
+            }
+        }
+        else {
+            selectedCards.append(index)
+
+        }
+        
+    }
+    
+    func checkForSet() -> Bool {
+        
+        var isASet = false
+        
+        if ((playableCards[selectedCards[0]].number == playableCards[selectedCards[1]].number && playableCards[selectedCards[1]].number == playableCards[selectedCards[2]].number) ||
+            (playableCards[selectedCards[0]].number != playableCards[selectedCards[1]].number && playableCards[selectedCards[1]].number != playableCards[selectedCards[2]].number && playableCards[selectedCards[0]].number != playableCards[selectedCards[2]].number))
+            {
+                if ((playableCards[selectedCards[0]].shape == playableCards[selectedCards[1]].shape && playableCards[selectedCards[1]].shape == playableCards[selectedCards[2]].shape) ||
+                    (playableCards[selectedCards[0]].shape != playableCards[selectedCards[1]].shape && playableCards[selectedCards[1]].shape != playableCards[selectedCards[2]].shape && playableCards[selectedCards[0]].shape != playableCards[selectedCards[2]].shape))
+                {
+                    if ((playableCards[selectedCards[0]].color == playableCards[selectedCards[1]].color && playableCards[selectedCards[1]].color == playableCards[selectedCards[2]].color) ||
+                        (playableCards[selectedCards[0]].color != playableCards[selectedCards[1]].color && playableCards[selectedCards[1]].color != playableCards[selectedCards[2]].color && playableCards[selectedCards[0]].color != playableCards[selectedCards[2]].color))
+                    {
+                        if ((playableCards[selectedCards[0]].shading == playableCards[selectedCards[1]].shading && playableCards[selectedCards[1]].shading == playableCards[selectedCards[2]].shading) ||
+                            (playableCards[selectedCards[0]].shading != playableCards[selectedCards[1]].shading && playableCards[selectedCards[1]].shading != playableCards[selectedCards[2]].shading && playableCards[selectedCards[0]].shading != playableCards[selectedCards[2]].shading))
+                        {
+                            isASet = true
+                        }
+                    }
+                }
+        }
+        
+        return isASet
+    }
+    
+    init() {
+        for number in 0..<3 {
+            for shape in 0..<3 {
+                for color in 0..<3 {
+                    for shading in 0..<3 {
+                        let card = Card(number: number, shape: shape, color: color, shading: shading)
+                        deck.append(card)
+                    }
+                }
+            }
+        }
+        
+        shuffle()
+        
+        let numPlayableCards = 12
+        
+        for _ in 0..<numPlayableCards {
+            playableCards.append(deck.removeFirst())
+        }
+        
+    }
+    
+
+    func shuffle() {
+        for index in 0..<deck.count {
+            let rand = Int(arc4random_uniform(UInt32(deck.count)))
+            let card = deck[index]
+            deck[index] = deck[rand]
+            deck[rand] = card
+            
+        }
+    }
+    
+
+    
+}
--- SetGame/ViewController.swift
+++ SetGame/ViewController.swift
@@ -11,15 +11,134 @@ import UIKit
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        updateViewFromModel()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+   
+    lazy var game = SetGame()
+    
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var cardSelectionLabel: UILabel!
+    
+    
+    @IBOutlet var cardButtons: [UIButton]!
+    @IBAction func NewGame(_ sender: UIButton) {
+        
+        game = SetGame()
+        cardSelectionLabel.text = ""
+        updateViewFromModel()
     }
+    
+    @IBAction func DealThreeMoreCardsButton(_ sender: UIButton) {
+        game.dealThreeMoreCards()
+        updateViewFromModel()
+        
+    }
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        
+        if let cardIndex = cardButtons.index(of : sender) {
+            
+            if (cardIndex < game.playableCards.count) && !(game.selectedCards.contains(cardIndex)) {
+                game.chooseCard(at : cardIndex)
+                updateViewFromModel()
+            }
+        }
+    }
+    
+    func updateViewFromModel() {
+    
+        for index in cardButtons.indices {
+            
+            let button = cardButtons[index]
+            button.layer.borderWidth = 0.0
 
+            if index < game.playableCards.count {
+                let card = game.playableCards[index]
+                
+                //TODO: Added test function here. Need to change it back and change button.setTitle to button.setAttributedTitle
+                let cardValue = cardFaceTestFunction(for : card)
+                button.setTitle(cardValue, for: UIControlState.normal)
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
 
+                if game.selectedCards.contains(index) {
+                    button.layer.borderWidth = 3.0
+                    button.layer.borderColor = UIColor.yellow.cgColor
+                }
+                
+                scoreLabel.text = "Score: \(game.score)"
+                updateCardSelectionLabel()
+
+            }
+            else {
+                
+                button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 0)
+                button.setAttributedTitle(nil, for: UIControlState.normal)
+                button.setTitle(nil, for: UIControlState.normal)
+
+            }
+            
+        }
+        
+    }
+    
+    func updateCardSelectionLabel() {
+        if game.selectedCards.count == 1 {
+            let card1 = game.playableCards[game.selectedCards[0]]
+            cardSelectionLabel.text = "Selection: \(cardFaceTestFunction(for: card1))"
+        }
+        if game.selectedCards.count == 2 {
+            let card1 = game.playableCards[game.selectedCards[0]]
+            let card2 = game.playableCards[game.selectedCards[1]]
+            cardSelectionLabel.text = "Selection: \(cardFaceTestFunction(for: card1)) + \(cardFaceTestFunction(for: card2))"
+        }
+        if game.selectedCards.count == 3 {
+            let card1 = game.playableCards[game.selectedCards[0]]
+            let card2 = game.playableCards[game.selectedCards[1]]
+            let card3 = game.playableCards[game.selectedCards[2]]
+            cardSelectionLabel.text = "Selection: \(cardFaceTestFunction(for: card1)) + \(cardFaceTestFunction(for: card2)) + \(cardFaceTestFunction(for: card3))"
+        }
+    }
+    
+    func cardFaceTestFunction(for card : Card) -> String{
+        
+        var val = ""
+        
+        val.append(String(card.number))
+        val.append(String(card.shape))
+        val.append(String(card.color))
+        val.append(String(card.shading))
+
+        return val
+    }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
